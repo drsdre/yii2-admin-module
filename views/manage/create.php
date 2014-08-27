28ba46341500b0ca8f4aa5243e0b8f5d2e53c655
Pavel Kilin <pavel@asdf404.net>
2014-08-27 16:29:55 +0700

    Fixed create form breadcrumbs

--- views/manage/create.php
+++ views/manage/create.php
@@ -8,7 +8,6 @@
 
 $this->title = $entity->labels[0];
 $this->params['breadcrumbs'][] = ['label' => $entity->labels[1], 'url' => ['index', 'entity' => $entity->id]];
-$this->params['breadcrumbs'][] = ['label' => Yii::t('admin', 'View'), 'url' => ['view', 'entity' => $entity->id, 'id' => $model->primaryKey]];
 $this->params['breadcrumbs'][] = Yii::t('admin', 'Creating');
 ?>
 <div class="model-create">
